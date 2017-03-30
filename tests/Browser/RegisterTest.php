588fcfc58416f0dc06c7fedf75485be4a0d8277d
Tyler Watkins <s3542686@student.rmit.edu.au>
2017-03-30 16:30:03 +1100

    Change registration test to handle more fields

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -7,19 +7,25 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class RegisterTest extends DuskTestCase
 {
-
+    
     use DatabaseMigrations;
-
+    
     public function test_user_successful_registration()
     {
         $this->browse(function ($browser) {
             $browser->visit('/register')
-                    ->type('name', 'Tyler Watkins')
-                    ->type('email', 'tjwato@gmail.com')
-                    ->type('password', 'password')
-                    ->type('password_confirmation', 'password')
-                    ->press('Register')
-                    ->assertPathIs('/home');
+            ->type('first_name', 'Aiden')
+            ->type('last_name','Garipoli')
+            ->type('email', 'email@gmail.com')
+            ->type('phone','0411122200')
+            ->type('address','124 La Trobe St')
+            ->type('city','Melbourne')
+            ->type('state','VIC')
+            ->type('post_code','3000')
+            ->type('password', 'password')
+            ->type('password_confirmation', 'password')
+            ->press('Register')
+            ->assertPathIs('/home');
         });
     }
 }
