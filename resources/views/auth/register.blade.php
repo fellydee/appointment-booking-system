1cadd078760808a545fd089a6fac6a56b543775d
Daniel Milner <s3542977@student.rmit.edu.au>
2017-03-31 12:10:24 +1100

    update background

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,4 +47,14 @@
         </div>
     </div>
 </div>
+
+<style>
+
+body {
+    background-image: url("http://senamalancha.com/(S(nlannpjrpoo3ajpflx5hfb2t))/logindata/bg-8.jpg");
+    background-size: cover;
+    background-color: rgba(0,0,0,.45);
+    background-blend-mode: multiply;
+}
+</style>
 @stop
