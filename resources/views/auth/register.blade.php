1392776dd4996878a2144b61e55189dd2206f6b8
Tyler Watkins <s3542686@student.rmit.edu.au>
2017-04-25 14:33:53 +1000

    Update registration validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -93,7 +93,7 @@
                     <div class="form-group">
                         <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
                         <div class="col-md-6">
-                            <input type="password" class="form-control" name="password_confirmation" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}">
+                            <input type="password" class="form-control" name="password_confirmation" required>
                         </div>
                     </div>
 
