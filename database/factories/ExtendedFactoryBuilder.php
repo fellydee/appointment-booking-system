c519cba45d5376f272daf9567749bf44721d3d23
Aiden Garipoli <s3542623@student.rmit.edu.au>
2017-05-26 13:17:21 +1000

    extended factory

---
+++ database/factories/ExtendedFactoryBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Eloquent\FactoryBuilder;
+
+class ExtendedFactoryBuilder extends FactoryBuilder
+{
+    public function __construct($class, $name, array $definitions, array $states, \Faker\Generator $faker)
+    {
+        parent::__construct($class, $name, $definitions, $states, $faker);
+    }
+
+    public function businessOwner()
+    {
+        return $this->create(['role' => 0]);
+    }
+
+    public function user()
+    {
+        return $this->create(['role' => 1]);
+    }
+
+    public function oneHour()
+    {
+        return $this->create(['duration' => 60]);
+    }
+
+    public function twoHour()
+    {
+        return $this->create(['duration' => 120]);
+    }
+}
