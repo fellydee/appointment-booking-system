241a8941d6a4731e6a7c14444ce5a495ea7b4d5b
Tyler Watkins <s3542686@student.rmit.edu.au>
2017-05-26 18:22:36 +1000

    Fix dropdown

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <title>{{ config('app.name') }}</title>
-    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.2.0.min.js"></script>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @yield('head')
     <script>
